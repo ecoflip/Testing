bed5e140d0ab18573f4e06f48ab65e5db10a460c
Kopec-Srandy <kopec.srandy42@gmail.com>
2022-12-02 19:06:40 +0100

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useMemo } from 'react';
+import { ExodusWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const endpoint = "https://solana-mainnet.g.alchemy.com/v2/575tomZNC-UI6pDC_9GR8kY4SIKrfYmc"
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new ExodusWalletAdapter()
+    ], []
+  );
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
 }
