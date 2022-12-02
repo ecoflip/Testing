e518ebc371fce3a995a8ef4e4442cc3f4a9119c8
Kopec-Srandy <kopec.srandy42@gmail.com>
2022-12-02 19:10:07 +0100

    sss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { useMemo } from 'react';
 import { ExodusWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 export default function App({ Component, pageProps }: AppProps) {
   const endpoint = "https://solana-mainnet.g.alchemy.com/v2/575tomZNC-UI6pDC_9GR8kY4SIKrfYmc"
